880d6f750c759a1a01e91d92a7677cbbfd302312
Kai <xktjcn@163.com>
2019-01-02 23:19:45 +0800

    Organize CSS and gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,2 @@
-var gulp = require("gulp"),
-  watch = require("gulp-watch");
-
-gulp.task("styles", function() {
-  return gulp
-    .src("./assets/styles/styles.css")
-    .pipe(gulp.dest("./temp/styles"));
-});
-
-gulp.task("watch", function() {
-  watch("./index.html", function() {
-    console.log("Gulp is running well!!");
-  });
-
-  watch("./assets/styles/**/*.css", function() {
-    gulp.start("styles");
-  });
-});
+require("./gulp/tasks/style");
+require("./gulp/tasks/watch");
