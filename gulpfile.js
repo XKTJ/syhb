3920147a5fe3fa5cbc092308d716ca230e0d06f2
Kai <xktjcn@163.com>
2019-01-02 18:58:14 +0800

    Gulp从4.0降级到3.9.1，因为部分命令失效，例如：gulp.start

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require("gulp"),
   watch = require("gulp-watch");
 
+gulp.task("styles", function() {
+  return gulp
+    .src("./assets/styles/styles.css")
+    .pipe(gulp.dest("./temp/styles"));
+});
+
 gulp.task("watch", function() {
   watch("./index.html", function() {
     console.log("Gulp is running well!!");
   });
 
   watch("./assets/styles/**/*.css", function() {
-    console.log("CSS file changed!!!");
+    gulp.start("styles");
   });
 });
